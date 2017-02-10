8faa30c0f95d87df849b92d3357304c38767912f
vagrant <vagrant@localhost.localdomain>
2017-02-10 19:38:49 +0000

    Add MongoDB package to default recipe

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+
+template '/etc/yum.repos.d/mongodb-org-2.6.repo' do
+  source 'mongodb-org-3.4.repo.erb'
+end
+
+package 'mongodb-org'
